6535cce9dddf7e509c1cdd3b9f1ecc723d326b9b
Gino Burdea <burdea.gino@gmail.com>
2024-10-12 00:26:40 +0300

    feat: make the register page functional

--- src/app/(auth)/inregistrare/page.tsx
+++ src/app/(auth)/inregistrare/page.tsx
@@ -1,43 +1,67 @@
 'use client'
 import Button from '@/app/components/Button'
 import Input from '@/app/components/Input'
+import submitForm from '@/utils/submitForm'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import ms from 'ms'
+import { setCookie } from 'cookies-next'
 
 export default function RegisterPage() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [fieldErrors, setFieldErrors] = useState({})
+    const [error, setError] = useState<string | null>(null)
+    const [loading, setLoading] = useState(false)
 
-    const [nameError, setNameError] = useState(null)
-    const [emailError, setEmailError] = useState(null)
-    const [passwordError, setPasswordError] = useState(null)
+    const router = useRouter()
 
-    const [error, setError] = useState(null)
+    const handleSuccess = (data: { name: string; email: string }) => {
+        const date30DaysInTheFuture = new Date(Date.now() + ms('30 days'))
+
+        setCookie('user.name', data.name, {
+            expires: date30DaysInTheFuture,
+        })
+        setCookie('user.email', data.email, {
+            expires: date30DaysInTheFuture,
+        })
+
+        router.push('/admin')
+    }
+
+    const register = submitForm(
+        '/users/register',
+        setLoading,
+        setFieldErrors,
+        setError,
+        handleSuccess
+    )
 
     return (
-        <>
+        <form onSubmit={register}>
             <h1 className="text-2xl font-bold mb-4">Inregistrare</h1>
 
             <Input
-                value={name}
-                setValue={setName}
                 label="Nume"
-                error={nameError}></Input>
+                type="text"
+                name="name"
+                error={fieldErrors.name}></Input>
             <Input
-                value={email}
-                setValue={setEmail}
                 label="Email"
                 type="email"
-                error={emailError}></Input>
+                name="email"
+                error={fieldErrors.email}></Input>
             <Input
-                value={password}
-                setValue={setPassword}
                 label="Parola"
                 type="password"
-                error={passwordError}></Input>
+                name="password"
+                error={fieldErrors.password}></Input>
+            <Input
+                label="Parola master"
+                type="password"
+                name="masterPassword"
+                error={fieldErrors.masterPassword}></Input>
 
-            <Button label="Inregistrare" onClick={() => {}}></Button>
+            <Button loading={loading} label="Inregistrare"></Button>
             {error && <p className="text-red-700 mt-4">Eroare: {error}</p>}
-        </>
+        </form>
     )
 }
