5a44d30318b9d0cb3a1fdb16d5901db78a7333c8
Gino Burdea <burdea.gino@gmail.com>
2024-10-14 10:50:03 +0300

    feat: make the photo preview feature functional

--- src/app/(app)/adauga-proiect/page.tsx
+++ src/app/(app)/adauga-proiect/page.tsx
@@ -4,6 +4,7 @@ import Input from '@/app/components/Input'
 import RadioInputs from '@/app/components/RadioInputs'
 import useSubmitForm from '@/utils/useSubmitForm'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { SyntheticEvent, useState } from 'react'
 
 export default function AddProjectPage() {
     const { error, fieldErrors, handleOnSubmit, loading } = useSubmitForm(
@@ -11,13 +12,28 @@ export default function AddProjectPage() {
         () => {}
     )
 
-    const photos = [
-        'https://placehold.co/250x200.png',
-        'https://placehold.co/250x400.png',
-        'https://placehold.co/250.png',
-        'https://placehold.co/250.png',
-        'https://placehold.co/250.png',
-    ]
+    const [photos, setPhotos] = useState<string[]>([])
+
+    const handleFiles = (event: SyntheticEvent<HTMLInputElement>) => {
+        console.log(event.currentTarget.files)
+
+        for (const file of event.currentTarget.files || []) {
+            const fileReader = new FileReader()
+
+            fileReader.addEventListener(
+                'load',
+                () => {
+                    setPhotos(photos => [
+                        ...photos,
+                        fileReader.result as string,
+                    ])
+                },
+                false
+            )
+
+            fileReader.readAsDataURL(file)
+        }
+    }
 
     return (
         <form onSubmit={handleOnSubmit}>
@@ -25,23 +41,21 @@ export default function AddProjectPage() {
 
             <p className="text-sm mb-1 font-bold">Imagini</p>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 mb-4">
-                {photos.map(photo => (
+                {photos.map((photo, photoIndex) => (
                     <div
                         className="w-full aspect-square bg-no-repeat bg-center bg-cover p-1 text-right"
                         style={{ backgroundImage: `url('${photo}')` }}
-                        key={photo}>
-                        <a className="p-1 hover:bg-gray-100 cursor-pointer transition-all">
-                            <i className="bi-x text-lg"></i>
-                        </a>
-                    </div>
+                        key={photoIndex}></div>
                 ))}
 
                 <label className="w-full aspect-square gap-2 p-2 flex justify-center items-center flex-col border-[1px] border-dashed border-blue-300 hover:bg-blue-50 cursor-pointer text-center">
                     <input
                         type="file"
+                        name="photos"
                         className="hidden"
                         accept="image/*"
                         multiple
+                        onChange={handleFiles}
                     />
 
                     <i className="bi-upload text-xl"></i>
