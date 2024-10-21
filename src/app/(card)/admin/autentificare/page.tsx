2f35dae41a6eed8e4488cc83438b598a2be6fa47
Gino Burdea <121102690+ginoburdea@users.noreply.github.com>
2024-10-21 17:41:52 +0300

    feat: link the register page to the login page and vice versa (#13)
    * feat: add a link to the register page on the login page
    
    * feat: add a link to the login page on the register page

--- src/app/(card)/admin/autentificare/page.tsx
+++ src/app/(card)/admin/autentificare/page.tsx
@@ -3,8 +3,9 @@ import Button from '@/components/Button'
 import Input from '@/components/Input'
 import { useRouter, useSearchParams } from 'next/navigation'
 import useSubmitForm from '@/utils/useSubmitForm'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useHandleAuthSuccess } from '@/utils/useHandleAuthSuccess'
+import Link from 'next/link'
 
 // Accepted query parameters:
 // error?: string (an error to show to the user)
@@ -29,6 +30,15 @@ export default function LoginPage() {
         }
     }, [])
 
+    const registerUrl = useMemo(() => {
+        const baseUrl = '/admin/inregistrare'
+
+        const nextParam = params.get('next')
+        if (nextParam === null) return baseUrl
+
+        return baseUrl + `?next=${nextParam}`
+    }, [params])
+
     return (
         <form onSubmit={handleOnSubmit}>
             <h1 className="text-2xl font-bold mb-4">Autentificare</h1>
@@ -44,8 +54,18 @@ export default function LoginPage() {
                 name="password"
                 error={fieldErrors.password}></Input>
 
-            <Button loading={loading} label="Autentificare"></Button>
-            {error && <p className="text-red-700 mt-4">Eroare: {error}</p>}
+            <div className="mb-4">
+                <Button loading={loading} label="Autentificare"></Button>
+            </div>
+
+            {error && <p className="text-red-700 mb-4">Eroare: {error}</p>}
+
+            <p>
+                Nu ai cont?{' '}
+                <Link className="v-link" href={registerUrl}>
+                    Inregistreaza-te acum
+                </Link>
+            </p>
         </form>
     )
 }
