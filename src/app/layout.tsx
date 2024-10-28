d47edb8aa5f6cd7c8a9ce9297b19e9a0b52684fe
Gino Burdea <burdea.gino@gmail.com>
2024-10-28 15:58:23 +0200

    feat: add desktop header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,53 @@
+import Link from 'next/link'
 import './globals.css'
+
+interface Menu {
+    title: string
+    link: string
+}
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
+    const menus: Menu[] = [
+        {
+            title: 'Acasa',
+            link: '/',
+        },
+        {
+            title: 'Acasa admin',
+            link: '/admin',
+        },
+        {
+            title: 'Inregistrare',
+            link: '/admin/inregistrare',
+        },
+        {
+            title: 'Autentificare',
+            link: '/admin/autentificare',
+        },
+    ]
+
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <nav className="bg-blue-50 p-4 mb-4 text-sm sticky top-0 shadow-sm z-50">
+                    <div className="gap-4 items-center justify-center sm:flex hidden">
+                        {menus.map(menu => (
+                            <Link
+                                key={menu.title}
+                                href={menu.link}
+                                className="hover:underline"
+                            >
+                                {menu.title}
+                            </Link>
+                        ))}
+                    </div>
+                </nav>
+                <main>{children}</main>
+            </body>
         </html>
     )
 }
