70b0ab9d5a6f646bd64e25d0e373f716e81f0173
Gino Burdea <burdea.gino@gmail.com>
2024-10-28 19:26:53 +0200

    feat: use the new api language options to get translated error messages

--- src/utils/getAxios.ts
+++ src/utils/getAxios.ts
@@ -1,9 +1,16 @@
 import axios from 'axios'
 
 export const getAxios = () => {
-    return axios.create({
+    const axiosInstance = axios.create({
         baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
         formSerializer: { indexes: null },
         withCredentials: process.env.NODE_ENV !== 'production',
     })
+
+    axiosInstance.interceptors.request.use(config => {
+        config.headers['accept-language'] = 'ro'
+        return config
+    })
+
+    return axiosInstance
 }
